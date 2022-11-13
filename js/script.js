3d4f86432205a54d65133bb3941b1eb6b73c192f
LanAd2 <tranthanhlan.ad2@gmail.com>
2022-11-13 10:15:52 +0700

    Implement the Header component to be sticky.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const navIcon = document.querySelector(".nav__icons");
 const navItems = document.querySelectorAll(".nav__item");
 const nav = document.querySelector(".nav");
-const body = document.body;
 
 navIcon.addEventListener("click", function () {
   nav.classList.toggle("nav--open");
