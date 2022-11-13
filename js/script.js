b933c9fc38be145fd2eabd824745dc6975d89118
LanAd2 <tranthanhlan.ad2@gmail.com>
2022-11-13 09:33:18 +0700

    Implement the icons in the nav bar fixed.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const navIcon = document.querySelector(".nav__icons");
+const navItems = document.querySelectorAll(".nav__item");
 const nav = document.querySelector(".nav");
+const body = document.body;
 
 navIcon.addEventListener("click", function () {
   nav.classList.toggle("nav--open");
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    nav.classList.remove("nav--open");
+  });
 });
